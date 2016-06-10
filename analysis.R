3a23176441b3cab046089b769d41e3fcf092176a
worldtravelnread <worldtravelnread@protonmail.com>
2016-06-10 13:18:30 -0500

    load files

--- analysis.R
+++ analysis.R
@@ -1,16 +1,6 @@
----
-title: "Reproducible Research: Peer Assessment 1"
-author: "Sharon Francisco"
-output: 
-  html_document:
-    keep_md: true
----
+## Reproducible Research - Project 1
 
 ## Installing packages and loading libraries
-
-This step is not required by the assignment, but since I used these packages, I included them in the report.
-
-``` {r loadLibraries, results = "hide"}
 if(!require(dplyr, quietly = TRUE)) install.packages("dplyr")
 library(dplyr)
 
@@ -19,117 +9,81 @@ library(lubridate)
 
 if(!require(ggplot2, quietly = TRUE)) install.packages("ggplot2")
 library(ggplot2)
-```
 
-The next step is not required, but I like to clear the variables in the environment before running new code.
+##workingDirChar <- "C:/Users/b1050549/Documents/RResearch_project1"
+workingDirChar <- "~/Documents/DataScience_JH/ReproducibleResearch/project1/RepData_PeerAssessment1"
+setwd(workingDirChar)
 
-``` {r clearEnvironment}
+## clear the environment
 rm(list = ls())
-```
 
 ## Loading and preprocessing the data
 
-The assignment does not require the code for downloading the file, but I included it for the sake of completeness.
+## 1. Load the data
+## fileUrl <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip"
+## download.file(fileUrl, destfile = "activity.zip", method = "curl")
+## dateDownloaded <- now()
 
-```{r downloadData}
-fileUrl <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip"
-download.file(fileUrl, destfile = "activity.zip", method = 
-                      "curl")
-dateDownloaded <- now()
-
-```
-
-This assignment makes use of data from a personal activity monitoring device. This device collects data at 5 minute intervals throughout the day. The data consists of two months of data from an anonymous individual collected during the months of October and November 2012 and include the number of steps taken in 5 minute intervals each day.
-
-The Activity Monitor Data was downloaded from <https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip> on `r dateDownloaded`.
-
-1. Load the data.
-
-```{r loadData}
 unzip("activity.zip")
 activityOrigDf <- read.csv("activity.csv", header = TRUE)
-```
 
-2. Process/transform the data (if necessary) into a format suitable for your analysis.
-
-- I use dplyr in the code so I tranformed the data to a data frame table.
-
-```{r createDfTbl}
+## 2. Process/transform the data (if necessary) into a format suitable 
+## for your analysis.
 activityOrigDfTbl <- tbl_df(activityOrigDf)
 print(activityOrigDfTbl)
 summary(activityOrigDfTbl)
-```
-
 
 ## What is mean total number of steps taken per day?
-1. Calculate the total number of steps taken per day.
-2. see below
-3. Calculate and report the mean and median of the total number of steps taken per day.
 
-```{r totalStepsByDay}
+## 1. Calculate the total number of steps taken per day.
+## 3. Calculate and report the mean and median of the total number of steps 
+## taken per day.
 totalStepsByDay <- activityOrigDfTbl %>%
         group_by(date) %>%
         summarize(totalSteps = sum(steps, na.rm = TRUE),
                   mean = mean(steps, na.rm = TRUE),
                   median = median(steps, na.rm = TRUE))
 print(totalStepsByDay)
-```
 
-2. Make a histogram of the total number of steps taken each day.
-
-```{r plot1}
+## 2. Make a histogram of the total number of steps taken each day.
 plot1 <- hist(totalStepsByDay$totalSteps, xlab = "Total Steps Each Day",
      main = "Plot 1: Frequency Distribution of Total Steps Taken Each Day")
-```
+
 
 
 ## What is the average daily activity pattern?
-1. Make a time series plot of the 5-minute interval (x-axis) and the average number of steps taken, averaged across all days (y-axis).
-
-```{r avgStepsByInterval}
+## Make a time series plot of the 5-minute interval (x-axis) and the 
+## average number of steps taken, averaged across all days (y-axis).
 avgStepsByInterval <- activityOrigDfTbl %>%
         group_by(interval) %>%
-        summarize(avgSteps = mean(steps, na.rm = TRUE), 
-                  totalSteps = sum(steps, na.rm = TRUE))
+        summarize(avgSteps = mean(steps, na.rm = TRUE), totalSteps = 
+                          sum(steps, na.rm = TRUE))
 print(avgStepsByInterval)
 
 plot2 <- with(avgStepsByInterval, plot(interval, avgSteps, type = "l", 
         xlab = "Time Interval", ylab = "Average Number of Steps",
         main = "Plot 2: Average Number of Steps by Time Interval"))
-```
 
-2. Which 5-minute interval, on average across all the days in the dataset, contains the maximum number of steps?
-
-```{r maxInterval}
 summary(avgStepsByInterval)
 maxInterval <- avgStepsByInterval %>%
         filter(avgSteps == max(avgSteps))
 print(maxInterval)
-```
-
-- Interval `r maxInterval$interval` has the maximum number of steps, both the average steps and the total steps.
 
 ## Imputing missing values
-1. Calculate and report the total number of missing values in the dataset (i.e., the total number of rows with **NA**s).
-
-```{r cntNAs}
+## 1. Calculate and report the total number of missing values in the dataset 
+## (i.e., the total number of rows with **NA**s).
 cntNAs <- length(which(is.na(activityOrigDfTbl$steps)))
-```
-- There are `r cntNAs` **NA**s in the dataset.
 
-- Although not required, the next block of code determines which days had **NAs**.
-
-```{r daysNA}
+## get the dates where steps are NA
 daysNA <- totalStepsByDay %>%
         filter(is.nan(mean))
 daysNA
-```
 
-2. Devise a strategy for filling in all of the missing values in the dataset. The strategy does not need to be sophisticated. For example, you could use the mean/median for that day, or the mean for that 5-minute interval, etc.
+## 2. Devise a strategy for filling in all of the missing values in the 
+## dataset. The strategy does not need to be sophisticated. For example, you 
+## could use the mean/median for that day, or the mean for that 5-minute 
+## interval, etc.
 
-- I decided to substitute the **NA**s with the average steps for the associated 5-minute interval.
-
-```{r imputedVectors}
 ## create an empty vector to store the returned values
 imputedVectNum <- vector(mode = "numeric", length = 0)
 stepTypeVect <- character(length = 0)
@@ -168,21 +122,21 @@ for(i in 1: totalObservations) {
 imputedStepsVect <- imputedVectNum
 head(imputedStepsVect)
 head(stepTypeVect)
-```
 
-3. Create a new dataset that is equal to the original dataset but with the missing data filled in.
+## 3. Create a new dataset that is equal to the original 
+## dataset but with the missing data filled in.
 
-```{r activityImputedDfTbl}
 ## add the imputedSteps and stepType vectors to the activity data
 activityImputedDfTbl <- activityOrigDfTbl %>%
         mutate(imputedSteps = imputedStepsVect, stepType = stepTypeVect)
 head(activityImputedDfTbl)
 summary(activityImputedDfTbl)
-```
 
-4. Make a histogram of the total number of steps taken each day, and calculate and report the **mean** and **median** total number of steps taken per day. 
 
-```{r imputedByDate}
+## 4. Make a histogram of the total number of steps taken each 
+## day, and calculate and report the **mean** and **median** 
+## total number of steps taken per day.
+
 ## summarize imputedSteps by date
 imputedByDate <- activityImputedDfTbl %>%
         group_by(date) %>%
@@ -197,32 +151,67 @@ imputedByDate <- activityImputedDfTbl %>%
                 deltaMedian = medianImputedSteps - medianOrigSteps)
 head(imputedByDate)
 
-plot3 <-hist(imputedByDate$totalImputedSteps, xlab = 
-        "Total Imputed Steps Each Day", main = 
-        "Plot 3: Frequency Distribution of Total Imputed Steps Taken Each Day")
-```
+## create histogram
+plot3 <-hist(imputedByDate$totalImputedSteps, xlab = "Total Imputed Steps Each Day", 
+     main = "Plot 3: Frequency Distribution of Total Imputed Steps Taken Each Day")
 
 
-Do these values differ from the estimates from the first part of the assignment? What is the impact of imputing missing data on the estimates of the total daily number of steps?
 
-```{r getDifferences}
+## Do these values differ from the estimates from the first 
+## part of the assignment? 
+
 ## get the differences calcultaed in imputedByDate for the days with NAs.
 differencesDfTbl <- imputedByDate %>%
         filter(is.nan(deltaAvg)) %>%
         select(date, deltaTotal, deltaAvg, deltaMedian)
 print(differencesDfTbl)
-```
 
-- The **NA**s affected the calculation for whole days, and since I imputed the missing values using the mean of the steps for the associated interval, the imputed values resulted in an increase in `r differencesDfTbl[1, "deltaTotal"]` steps for each day. 
 
-- The impact of imputing the steps for the total steps by interval is shown below.
+## What is the impact of imputing 
+## missing data on the estimates of the total daily number 
+## of steps?
+                  
+## byIntervalByStepType <- activityImputedDfTbl %>%
+##        group_by(interval, stepType) %>%
+##        summarize(totalOrigSteps = sum(steps, na.rm = TRUE), 
+##                  totalImputedSteps = sum(imputedSteps))
 
-```{r totalImputedComparison}
+## head(byIntervalByStepType)
+
+## plot4 <- ggplot(byIntervalByStepType, aes(interval, totalImputedSteps))
+## plot4 <- plot4 + geom_line(aes(color = stepType)) + labs(x = 
+##                "Interval", y = "Total Steps",
+##                title = "Comparison of Total Original and Imputed Steps ")
+## print(plot4)
+
+## byDateByStepType <- activityImputedDfTbl %>%
+##        group_by(date, stepType) %>%
+##        summarize(totalOrigSteps = sum(steps, na.rm = TRUE), 
+##                  totalImputedSteps = sum(imputedSteps))
+## head(byDateByStepType)
+
+
+
+## plot4a <- qplot(totalImputedSteps, data = byDateByStepType, fill = stepType,
+##                binwidth = 150)
+## print(plot4a)
+
 totalStepsByInterval <- activityImputedDfTbl %>%
         group_by(interval) %>%
         summarize(totalOrigSteps = sum(steps, na.rm = TRUE), 
                   totalImputedSteps = sum(imputedSteps))
 head(totalStepsByInterval)
+
+## plot4 <- with(totalStepsByInterval, plot(interval, totalOrigSteps, pch = 19,
+##        xlab = "Time Interval", ylab = "Total Steps",
+##        main = "Plot 4: Comparison of Total Number of Original and Imputed Steps"),
+##        col = rgb(0, 0, 0, 0.15))
+## plot4 <- with(totalStepsByInterval, points(interval, totalImputedSteps, pch = 19,
+##        col = rgb(0, 0, 1, 0.50)))
+## plot4 <- legend(x=2000, y = 10000, pch = 19, cex = 1, col = c("black", "blue"), 
+##        legend = c("Original", "Imputed"), bty = "n")
+
+
 plot4 <- ggplot(totalStepsByInterval, aes(interval, totalImputedSteps))
 plot4 <- plot4 + geom_line(aes(color = "Imputed"))  + geom_line(aes(interval, 
         totalOrigSteps, color = "Original")) + labs(x = "Interval", 
@@ -230,17 +219,21 @@ plot4 <- plot4 + geom_line(aes(color = "Imputed"))  + geom_line(aes(interval,
         title = "Plot 4: Comparison of Total Original and Imputed Steps ") +
         theme(legend.position = "right")
 print(plot4)
-```
+
+
 
 ## Are there differences in activity patterns between weekdays and weekends?
 
-1.Create a new factor variable in the dataset with two levels -- "weekday" and "weekend" indicating whether a given date is a weekday or weekend day.
+## 1.Create a new factor variable in the dataset with two levels 
+## -- "weekday" and "weekend" indicating whether a given date is 
+## a weekday or weekend day.
 
-```{r wkdayActivityImputedDfTbl}
+
 ## create a table with the weekday
 wkdayActivityImputedDfTbl <- activityImputedDfTbl %>%
         mutate(weekday = weekdays(as_date(date)))
 head(wkdayActivityImputedDfTbl)
+               
 
 ## assign an empty vector
 dayTypeVect <- vector("character", length = 0)
@@ -265,21 +258,22 @@ for(j in 1:totalObservations) {
 dayTypeActivityImputedDfTbl <- wkdayActivityImputedDfTbl %>%
         mutate(dayType = dayTypeVect)
 head(dayTypeActivityImputedDfTbl)
-```
 
-2.Make a panel plot containing a time series plot (i.e.  type = "l" ) of the 5-minute interval (x-axis) and the average number of steps taken, averaged across all weekday days or weekend days (y-axis). 
+## 2.Make a panel plot containing a time series plot (i.e.  
+## type = "l" ) of the 5-minute interval (x-axis) and the 
+## average number of steps taken, averaged across all weekday 
+## days or weekend days (y-axis). 
 
-```{r byIntervalByDayType}
 byIntervalByDayType <- dayTypeActivityImputedDfTbl %>%
         group_by(interval, dayType) %>%
         summarize(avgDayTypeImputedStep = mean(imputedSteps))
 
-head(byIntervalByDayType)
-
 plot5 <- ggplot(byIntervalByDayType, aes(interval, avgDayTypeImputedStep))
 plot5 <- plot5 + geom_line() + facet_grid(dayType ~ .) + labs(title = 
-                "Average Number of Imputed Steps by Time Interval", x = 
-                        " Time Interval", y = "Number of Imputed Steps")
+                "Plot 5: Average Number of Imputed Steps by Time Interval", 
+                x = " Time Interval", y = "Number of Imputed Steps")
 print(plot5)
-```
+
+
+
 
